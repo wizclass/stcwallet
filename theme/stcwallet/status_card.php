7bac98c024df8514e34474fab15f2f2d75d44ff0
SeongKon <canoa950327@gmail.com>
2023-12-21 11:26:08 +0900

    [추가] 대시보드 카드 USDT 환산

--- theme/stcwallet/status_card.php
+++ theme/stcwallet/status_card.php
@@ -2,8 +2,18 @@
     <div class="status_card_wrap esgc">
         <h5><?= ASSETS_CURENCY ?> 자산 현황</h5>
         <div class="quantity_wrap">
-            <p class="esgc_quantity"><?= $shift_total_token_balance ?> <span class="currency"><?= ASSETS_CURENCY ?></span></p>
-            <!-- <p class="price"><?= $total_token_balance_krw ?> <?= BALANCE_CURENCY ?></p> -->
+
+            <div class="row">
+                <p class="esgc_quantity col-6">
+                    <?= $shift_total_token_balance ?> <span class="currency"><?= ASSETS_CURENCY ?></span>
+                </p>
+                <div class="col-6 usdt_price" style="display: flex; flex-direction: column; text-align: right; font-size: 18px;">
+                    <div>1 STC = 1USDT</div>
+                    <div>1 USDT = <?= shift_auto($coin['usdt_krw'], ASSETS_CURENCY) ?> <?= BALANCE_CURENCY ?></div>
+                </div>
+
+            </div>
+            <p class="price"><?= shift_auto($total_token_balance * $coin['usdt_krw'], ASSETS_CURENCY) ?> <?= BALANCE_CURENCY ?></p>
         </div>
         <div class="link_btn_wrap">
             <a class="deposit" href="javascript:link('deposit','esgc');">입금</a>
